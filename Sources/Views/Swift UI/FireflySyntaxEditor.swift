41c931e5c492b45b8ed3d5a1cfd9aee05f00288f
Zachary Lineman <zachary.lineman@gmail.com>
2021-07-22 13:11:38 -0400

    Removed Comment

--- Sources/Views/Swift UI/FireflySyntaxEditor.swift
+++ Sources/Views/Swift UI/FireflySyntaxEditor.swift
@@ -180,9 +180,6 @@ public struct FireflySyntaxEditor: ViewRepresentable {
         if view.isHorizontalScrollingEnabled != allowHorizontalScroll.wrappedValue {
             view.setIsHorizontalScrollAllowed(isAllowed: allowHorizontalScroll.wrappedValue)
         }
-
-
-//        context.coordinator.wrappedView.setup(theme: theme.wrappedValue, language: language.wrappedValue, font: fontName.wrappedValue, offsetKeyboard: false, keyboardOffset: 0, dynamicGutter: dynamicGutter.wrappedValue, gutterWidth: gutterWidth.wrappedValue, placeholdersAllowed: placeholdersAllowed.wrappedValue, linkPlaceholders: linkPlaceholders.wrappedValue, lineNumbers: lineNumbers.wrappedValue, fontSize: fontSize.wrappedValue, allowHorizontalScroll: allowHorizontalScroll.wrappedValue)
     }
     #endif
     
