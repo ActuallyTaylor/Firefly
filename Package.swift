c74d947c42cdd975c8834f9485e45e698c356678
ActuallyTaylor <zachary.lineman@gmail.com>
2023-05-06 13:20:49 -0400

    Started work

--- Package.swift
+++ Package.swift
@@ -18,6 +18,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.7.1"),
+        .package(url: "https://github.com/alex-pinkus/tree-sitter-swift", from: "0.3.6")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
